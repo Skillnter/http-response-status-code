69f241259bf66b826832499a12d125ce0735240d
skillnter <skillhimanshubansal@gmail.com>
2021-11-07 03:47:46 +0530

    UPDATE FIXES FOR CODE STYLE IN TEST FILE
    * Fixed the code style issues listed in codecy.com

--- test/index-spec.js
+++ test/index-spec.js
@@ -1,29 +1,29 @@
-var assert = require('assert');
-var STATUS_CODES = require('../index.js');
-describe('status_code', function() {
-    it('print status code', function () {
+var assert = require("assert");
+var STATUS_CODES = require("../index.js");
+describe("status_code", function() {
+    it("print status code", function () {
         var result = STATUS_CODES.OK;
         assert.equal(result, 200);
     });
-    it('print status message', function () {
+    it("print status message", function () {
         var result = STATUS_CODES.getStatusDescription(200);
         assert.equal(result, "OK");
     });
-    it('print status name', function () {
+    it("print status name", function () {
         var result = STATUS_CODES.getStatusName(200);
         assert.equal(result, "OK");
     });
-    it('print status code from status name', function () {
+    it("print status code from status name", function () {
         var result = STATUS_CODES.getStatusCode("OK");
         assert.equal(result, 200);
     });
-    it('print status message fail', function () {
-        assert.throws(()=>{STATUS_CODES.getStatusDescription(0)}, Error);
+    it("print status message fail", function () {
+        assert.throws(() => {STATUS_CODES.getStatusDescription(0);}, Error);
     });
-    it('print status name fail', function () {
-        assert.throws(()=>{STATUS_CODES.getStatusName(0)}, Error);
+    it("print status name fail", function () {
+        assert.throws(() => {STATUS_CODES.getStatusName(0);}, Error);
     });
-    it('print status code from status name fail', function () {
-        assert.throws(()=>{STATUS_CODES.getStatusCode("NOT OK")}, Error);
+    it("print status code from status name fail", function () {
+        assert.throws(() => {STATUS_CODES.getStatusCode("NOT OK");}, Error);
     });
 });
