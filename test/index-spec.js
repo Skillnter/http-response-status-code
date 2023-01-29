f7d32197c3ec752683de56c15f01271f8ff71967
Skillnter <skillhimanshubansal@gmail.com>
2023-01-30 01:21:20 +0530

    ADD ENUM FOR STATUS CODES
    * Added enum for status codes `CODES`
    * Updated functions `getStatusCode` and `getStatusName` to ignore
    MODULE's key `CODES`
    * Updated testcases

--- test/index-spec.js
+++ test/index-spec.js
@@ -1,29 +1,36 @@
 var assert = require("assert");
 var STATUS_CODES = require("../index.js");
 describe("status_code", function() {
-    it("print status code", function () {
+    it("Should print status code", function () {
         var result = STATUS_CODES.OK;
         assert.equal(result, 200);
     });
-    it("print status message", function () {
+    it("Should print status message", function () {
         var result = STATUS_CODES.getStatusDescription(200);
         assert.equal(result, "OK");
     });
-    it("print status name", function () {
+    it("Should print status name", function () {
         var result = STATUS_CODES.getStatusName(200);
         assert.equal(result, "OK");
     });
-    it("print status code from status name", function () {
+    it("Should print status code from status name", function () {
         var result = STATUS_CODES.getStatusCode("OK");
         assert.equal(result, 200);
     });
-    it("print status message fail", function () {
+    it("Should print status code from code variable", function () {
+        var result = STATUS_CODES.CODES.HTTP_CODE_200;
+        assert.equal(result, 200);
+    });
+    it("Should throw error when getStatusDescription get wrong code", function () {
         assert.throws(() => {STATUS_CODES.getStatusDescription(0);}, Error);
     });
-    it("print status name fail", function () {
+    it("Should throw error when getStatusName get wrong code", function () {
         assert.throws(() => {STATUS_CODES.getStatusName(0);}, Error);
     });
-    it("print status code from status name fail", function () {
+    it("Should throw error when getStatusCode get wrong name", function () {
         assert.throws(() => {STATUS_CODES.getStatusCode("NOT OK");}, Error);
+    });
+    it("Should throw error when getStatusCode get name as CODES", function () {
+        assert.throws(() => {STATUS_CODES.getStatusCode("CODES");}, Error);
     });
 });
